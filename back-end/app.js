55d64f4c7ae57133899de165bc04d818c60b68e9
jonas18121 <jonasxtrm971@gmail.com>
2021-01-09 18:51:51 +0100

    les erreurs de CORS

--- back-end/app.js
+++ back-end/app.js
@@ -2,9 +2,21 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * Dans ce middleware, on ne met pas de route en premier argument, 
+ * afin que tous les autres middleware puisse en bénéficier de ce header
+ * et on aura plus d'erreurs de CORS
+ */
+app.use((request, response, next) => {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
 /* 
-    Le premier argument est la route qui permettra de recevoir le contenu de la constante stuff
-    l'addresse absolue est : 'http://localhost:3000/api/stuff',
+    Le premier argument est la route ( endpoint ) qui permettra de recevoir le contenu de la constante stuff
+    l'addresse absolue est pour l'instant : 'http://localhost:3000/api/stuff',
     notre partie front-end pourra recupéré le contenu de la constante stuff, a cette adresse absolue
 */
 app.use('/api/stuff', (request, response, next) => {
