8b2398be67e12d6f095ec59129dd37c6207884f0
jonas18121 <jonasxtrm971@gmail.com>
2021-01-18 16:15:48 +0100

    création login pour user

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -26,9 +26,33 @@ exports.signup = (request, response, next) => {
                 .catch(error => response.status(400).json({ error}));
             ;
         })
-        .catch(error => response.status(500).json({ error}));
+        .catch(error => response.status(500).json({ error}))
+    ;
 };
 
 exports.login = (request, response, next) => {
     
+    User.findOne({ email: request.body.email })
+        .then(user =>{
+
+            // si aucun user est trouvé dans la bdd, avec le mail provenant du formulaire, on lance une erreur 401
+            if(!user){
+                return response.status(401).json({ error: 'Utilisateur non trouvé ! '});
+            }
+
+            //si le mail est bon, on compare le password hashé provenant du formulaire avec celui de la bdd
+            bcrypt.compare(request.body.password, user.password)
+                .then(valid => {
+                    if(!valid){
+                        return response(401).json({ error: 'mot de passe incorrect' });
+                    }
+                    response.status(200).json({ 
+                        userId: user._id,
+                        token:'TOKEN'
+                    });
+                })
+                .catch(error => response.status(500).json({ error}))
+            ;
+        })
+        .catch(error => response.status(500).json({ error}));
 };
